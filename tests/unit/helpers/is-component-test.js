333d1f5cf6026d256f6195c717fd77944d7ac7ea
Xavier Cambar <xcambar@gmail.com>
2016-01-18 18:41:40 +0100

    Updates to getOwner

--- tests/unit/helpers/is-component-test.js
+++ tests/unit/helpers/is-component-test.js
@@ -1,26 +1,20 @@
 import Ember from 'ember';
 import { compute } from '../../../helpers/is-component';
-import { module, test } from 'qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-var container, registry;
-
-module('Unit | Helper | is component', {
-  beforeEach() {
-    registry = new Ember.Registry();
-    container = registry.container();
-    registry.register('component-lookup:main', Ember.ComponentLookup);
-  }
+moduleForComponent('helper:is-component', 'Unit | Helper | is component', {
+  integration: true
 });
 
 test('it returns true for a component', function(assert) {
-  registry.register('component:my-component', Ember.Component.extend());
-  assert.ok(compute.call({ container: container }, ['my-component']), 'true when the component exists');
-  assert.notOk(compute.call({ container: container }, ['my-non-component']), 'false when the component exists');
+  this.register('component:my-component', Ember.Component.extend());
+  assert.ok(compute.call(this, ['my-component']), 'true when the component exists');
+  assert.notOk(compute.call(this, ['my-non-component']), 'false when the component exists');
 });
 
 test('it returns true for a component with only a template', function(assert) {
-  registry.register('template:components/my-template', hbs`yep`, { instantiate: false });
-  assert.ok(compute.call({ container: container }, ['my-template']), 'true when a component template exists');
-  assert.notOk(compute.call({ container: container }, ['my-non-template']), 'false when a component template does not exist');
+  this.register('template:components/my-template', hbs`yep`, { instantiate: false });
+  assert.ok(compute.call(this, ['my-template']), 'true when a component template exists');
+  assert.notOk(compute.call(this, ['my-non-template']), 'false when a component template does not exist');
 });
