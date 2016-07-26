6287764417997baa2b84cbdbb6fd0070bd25cab1
Xavier Cambar <xcambar@gmail.com>
2016-07-26 18:45:47 +0200

    Compatible with Ember 2.7.0

--- addon/helpers/is-component.js
+++ addon/helpers/is-component.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 import getOwner from 'ember-getowner-polyfill';
 
 export function compute([name]) {
-  return !!getOwner(this).lookup('component-lookup:main').lookupFactory(name);
+  const owner = getOwner(this);
+  const lookup = owner.lookup('component-lookup:main');
+  if (!lookup.componentFor) {
+    return !!lookup.lookupFactory(name);
+  }
+  return !!lookup.componentFor(name, owner);
 }
 
 export default Ember.Helper.extend({
