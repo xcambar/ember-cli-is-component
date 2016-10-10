4d19039d7b4e3beffb54969f08ffff1dbb252b76
Xavier Cambar <xcambar@gmail.com>
2016-10-10 23:06:31 +0200

    Doesn't break on null/empty values

--- addon/helpers/is-component.js
+++ addon/helpers/is-component.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 import getOwner from 'ember-getowner-polyfill';
 
 export function compute([name]) {
+  name = (name || '').trim();
+  if (!name) {
+    return false;
+  }
   const owner = getOwner(this);
   const lookup = owner.lookup('component-lookup:main');
   if (!lookup.componentFor) {
