544bf7a87689015c4f54c6697093d9f2b0d7b1ea
Edward Faulkner <ef@alum.mit.edu>
2017-03-17 20:24:59 -0400

    Fix deprecation from ember-getowner-polyfill

--- addon/helpers/is-component.js
+++ addon/helpers/is-component.js
@@ -1,12 +1,11 @@
 import Ember from 'ember';
-import getOwner from 'ember-getowner-polyfill';
 
 export function compute([name]) {
   name = (name || '').trim();
   if (!name) {
     return false;
   }
-  const owner = getOwner(this);
+  const owner = Ember.getOwner(this);
   const lookup = owner.lookup('component-lookup:main');
   if (!lookup.componentFor) {
     return !!lookup.lookupFactory(name);
